be3fce9b181d433751e09aed7e9bfe2bebc33bd9
Solomon <sholasolomon@yahoo.com>
2023-11-21 01:11:15 +0000

    Excel package installation

--- config/app.php
+++ config/app.php
@@ -168,6 +168,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        'Excel' => Maatwebsite\Excel\Facades\Excel::class, //export to excel
     ])->toArray(),
 
     /*
@@ -183,6 +184,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
+        'Excel' => Maatwebsite\Excel\Facades\Excel::class, //export to excel
     ])->toArray(),
 
 ];
